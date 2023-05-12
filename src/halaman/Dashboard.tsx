20db1d83901c76fa37d98b690b5ceb9dce570681
Johanes Indra Pradana Pao <johanes.pao@gmail.com>
2023-05-12 14:05:19 +0700

    Menghapus catatan

--- src/halaman/Dashboard.tsx
+++ src/halaman/Dashboard.tsx
@@ -7,10 +7,3 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
-
-import PenerimaanBarang from "../halaman/PenerimaanBarang";
-import Stok from "../halaman/Stok";
-import KetersediaanStok from "../halaman/KetersediaanStok";
-import BuyingProposal from "../halaman/BuyingProposal";
-import LabaRugiToko from "../halaman/LabaRugiToko";
-import KelayakanTokoBaru from "../halaman/KelayakanTokoBaru";
