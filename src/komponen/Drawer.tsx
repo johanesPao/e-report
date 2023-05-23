4ce69c60061c1adca0152fa2a9220f5aeba58567
Johanes Indra Pradana Pao <johanes.pao@gmail.com>
2023-05-23 14:51:42 +0700

    Perubahan desain tata letak drawer

--- src/komponen/Drawer.tsx
+++ src/komponen/Drawer.tsx
@@ -13,11 +13,40 @@ import InputStok from "./kontenDrawer/InputStok";
 import InputKetersediaanStok from "./kontenDrawer/InputKetersediaanStok";
 import InputBuyingProposal from "./kontenDrawer/InputBuyingProposal";
 import InputLabaRugiToko from "./kontenDrawer/InputLabaRugiToko";
+import { useEffect, useState } from "react";
 
 const DrawerInput = () => {
   const halaman = useAppSelector(getHalaman);
   const drawerTerbuka = useAppSelector(getDrawerTerbuka);
   const dispatch = useAppDispatch();
+  const [judulDrawer, setJudulDrawer] = useState("");
+
+  useEffect(() => {
+    switch (halaman) {
+      case "penjualan":
+        setJudulDrawer("Input Parameter Penjualan");
+        break;
+      case "penerimaanBarang":
+        setJudulDrawer("Input Parameter Penerimaan Barang");
+        break;
+      case "stok":
+        setJudulDrawer("Input Parameter Stok");
+        break;
+      case "ketersediaanStok":
+        setJudulDrawer("Input Parameter Ketersediaan Stok");
+        break;
+      case "buyingProposal":
+        setJudulDrawer("Input Parameter Buying Proposal");
+        break;
+      case "labaRugiToko":
+        setJudulDrawer("Input Parameter Laba Rugi Toko");
+        break;
+      default:
+        setJudulDrawer("");
+        break;
+    }
+  }, [halaman]);
+
   const renderInput = () => {
     switch (halaman) {
       case "penjualan":
@@ -40,17 +69,19 @@ const DrawerInput = () => {
   return (
     <Drawer
       opened={drawerTerbuka}
+      title={judulDrawer}
       onClose={() => dispatch(setDrawerTerbuka(false))}
-      position="right"
+      position="top"
       overlayProps={{ opacity: 0.5, blur: 2 }}
       size="560px"
       withCloseButton={false}
       closeOnEscape={false}
       transitionProps={{
-        transition: "pop-bottom-right",
+        transition: "scale-y",
         duration: 250,
-        timingFunction: "ease-out",
+        timingFunction: "ease-in",
       }}
+      trapFocus={false}
     >
       {renderInput()}
     </Drawer>
