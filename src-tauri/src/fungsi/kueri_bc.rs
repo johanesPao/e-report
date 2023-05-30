82000c8aa3eeebace1239d74e4ac796c3653c9cc
Johanes Indra Pradana Pao <johanes.pao@gmail.com>
2023-05-30 12:04:16 +0700

    Kueri serta konversi data penjualan ke dataframe polars dan refaktor struct Pengguna

--- src-tauri/src/fungsi/kueri_bc.rs
+++ src-tauri/src/fungsi/kueri_bc.rs
@@ -1,4 +1,5 @@
 use crate::db::mssql;
+use crate::struktur::*;
 
 pub async fn kueri_umum(kueri: String) -> Result<Vec<Vec<String>>, Box<dyn std::error::Error>> {
     let mut data = Vec::new();
@@ -19,6 +20,42 @@ pub async fn kueri_umum(kueri: String) -> Result<Vec<Vec<String>>, Box<dyn std::
                 }
             }
             data.push(vektor_baris);
+        }
+    }
+    Ok(data)
+}
+
+pub async fn kueri_penjualan(
+    kueri: String,
+) -> Result<Vec<DataPenjualan>, Box<dyn std::error::Error>> {
+    let mut data = Vec::new();
+    let hasil_kueri = &mssql::kueri(kueri).await?[0];
+    if hasil_kueri.len() > 0 {
+        let jumlah_baris_data = hasil_kueri.len();
+        for baris in 0..jumlah_baris_data {
+            let no_entry = hasil_kueri[baris].get(0);
+            let post_date = hasil_kueri[baris].get(1);
+            let system_created_at = hasil_kueri[baris].get(2);
+            let store_dim = hasil_kueri[baris].get(3).map(|teks: &str| teks.to_string());
+            let loc_code = hasil_kueri[baris].get(4).map(|teks: &str| teks.to_string());
+            let no_dokumen = hasil_kueri[baris].get(5).map(|teks: &str| teks.to_string());
+            let source_no = hasil_kueri[baris].get(6).map(|teks: &str| teks.to_string());
+            let brand_dim = hasil_kueri[baris].get(7).map(|teks: &str| teks.to_string());
+            let oricode = hasil_kueri[baris].get(8).map(|teks: &str| teks.to_string());
+            let ukuran = hasil_kueri[baris].get(9).map(|teks: &str| teks.to_string());
+            let data_penjualan = DataPenjualan {
+                no_entry,
+                post_date,
+                system_created_at,
+                store_dim,
+                loc_code,
+                no_dokumen,
+                source_no,
+                brand_dim,
+                oricode,
+                ukuran,
+            };
+            data.push(data_penjualan);
         }
     }
     Ok(data)
